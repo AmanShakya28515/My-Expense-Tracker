c5bb8ccb78f1cb8a801f2a391512a199919638ad
Aman Shakya <aman28515@gmail.com>
2025-06-23 18:47:42 +0545

    made chnages

--- ExpTracker/ExpTracker/wsgi.py
+++ ExpTracker/ExpTracker/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ExpTracker.settings')
+settings_model = 'ExpTracker.demployment' if 'WEBSITE_HOSTNAME' in os.environ else 'ExpTracker.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_model)
 
 application = get_wsgi_application()
